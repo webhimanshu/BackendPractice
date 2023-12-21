83ffad75d6b61d5e62dbc89f2916049238a71a43
webhimanshu <himanshu.sharma@napses.com>
2023-12-21 18:15:34 +0530

    ui: littke confih

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 app.get('/', (req, resp)=>{
-  resp.send('Hello World From Express Server');
+  resp.send({data:'Hello World From Express Server'});
 });
 
-app.listen(3000, function(){
+
+app.post('/conversations', (req, resp)=>{
+  console.log(req.query.msg);
+  console.log(req.body)
+  console.log(req.headers['authorization']);
+  resp.send({data:'Hello World Post From Express Server'});
+});
+
+app.listen(3001, function(){
     console.log("Server in Running");
 })
